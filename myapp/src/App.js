3886bd63fd656e2a3a02f067c7ef7355bfff4272
ignacio <ihelocarraco@gmail.com>
2024-12-31 06:12:32 -0300

    terminada primera seccion

---
+++ myapp/src/App.js
@@ -0,0 +1,16 @@
+import logo from './logo.svg';
+import './App.css';
+import QuienesSomos from './Secciones/QuienesSomos';
+
+
+function App() {
+  return (
+      <>
+          <QuienesSomos/>
+          <h1>sA</h1>
+          <h1>aA</h1>
+      </>
+  );
+}
+
+export default App;
